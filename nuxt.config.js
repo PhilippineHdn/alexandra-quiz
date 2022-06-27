7993afe24d17660b62527bd5b44f08f1627356f5
Hugo Larrousse <hugo.larrousse@gmail.com>
2022-06-27 17:49:56 +0700

    change config

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: {
-    base: '/'
+    base: '/alexandra-quiz/'
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
